8cc57ee41e4f1cb2a7caeb63ced8075aa5525152
Cysou <cysou3@laposte.net>
2018-02-19 08:09:46 +0100

    binder creation

---
+++ Binder.py
@@ -0,0 +1,12 @@
+###GLOBAL###
+dico_bind = {}
+
+
+###FUNCTIONS###
+def bind(letter, function):
+    dico_bind[letter] = function
+
+def unbind(letter):
+    dico_key.pop(letter, None)
+
+def event()
--- constants.py
+++ constants.py
@@ -1,7 +1,15 @@
+from pygame.locals import *
+
 #main
 root_width = 1200
 root_height = 675
 timer_clock = 60 #fps max
+
+#Binder
+dico_key = {"a":K_a,"z":K_z,"e":K_e,"r":K_r,"t":K_t,"y":K_y,"u":K_u,"i":K_i,
+            "o":K_o,"p":K_p,"q":K_q,"s":K_s,"d":K_d,"f":K_f,"g":K_g,
+            "h":K_h,"j":K_j,"k":K_k,"l":K_l,"m":K_m,"w":K_w,"x":K_x,"c":K_c,
+            "v":K_v,"b":K_b,"n":K_n}
 
 #general
 g = 9.81
